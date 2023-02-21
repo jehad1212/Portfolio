1f048673114f862db1e2d9b62e4140528551a5ea
jehad1212 <71128110+jehad1212@users.noreply.github.com>
2023-02-21 21:22:09 +1100

    Create bit_reverse.v

---
+++ Verilog/bit_reverse.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    always @(*) begin
+        for(int i=99;i>=0;i=i-1) begin
+            out[99-i] = {in[i]};
+        end
+    end
+
+endmodule
